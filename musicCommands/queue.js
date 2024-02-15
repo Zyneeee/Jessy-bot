24ecb683ea432369fe5b353809012f9426a8023a
Zyneeee <157526994+Zyneeee@users.noreply.github.com>
2024-02-16 00:53:30 +0700

    Update queue.js

--- musicCommands/queue.js
+++ musicCommands/queue.js
@@ -15,7 +15,7 @@ module.exports = {
      .setAuthor({
           name: 'Queue',
           iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488721001398333/queue.png?ex=656b6a2e&is=6558f52e&hm=7573613cbb8dcac83ba5d5fc55ca607cf535dd117b4492b1c918d619aa6fd7ad&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/'
         })
       .setDescription(queue.map((song, index) => `**${index + 1}.** ${song.searchQuery}`).join('\n'));
 
