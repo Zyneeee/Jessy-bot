214d253cf8240e47fe7c83b25796379a5cc86553
Zyneeee <157526994+Zyneeee@users.noreply.github.com>
2024-02-15 23:52:51 +0700

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -14,7 +14,7 @@ module.exports = {
       .setAuthor({
         name: 'Hey it Me Jessy Im here to Help You',
         iconURL: 'https://cdn.discordapp.com/attachments/1187377061959045181/1207719172570619915/95c842110d8d7e60a852fdc14150c771.jpg?ex=65e0ab21&is=65ce3621&hm=74c8324881efbb0899ed80d6f5a92ae11b255d877dc0549f7d1b17af422fbd1c&', 
-        url: 'https://discord.gg'
+        url: 'https://discord.gg/'
     })
      
       .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [Zxy.]**\n\n__**COMMANDS :**__ `)
@@ -28,7 +28,7 @@ module.exports = {
         // Music commands category
         {
           name: '▶️  Music',
-          value: '```play/p| stop/s | history/h | volume/v | pause | resume | 24/7``',
+          value: '```play/p| skip/s | history/h | volume/v | pause/stop | resume | 24/7``',
           inline: true,
         },
         //fun category
@@ -61,17 +61,17 @@ module.exports = {
 
     const button1 = new ButtonBuilder()
       .setLabel('YouTube')
-      .setURL('https://www.youtube.com')
+      .setURL('https://www.youtube.com/')
       .setStyle(ButtonStyle.Link);
 
     const button2 = new ButtonBuilder()
       .setLabel('Discord')
-      .setURL('https://discord.gg')
+      .setURL('https://discord.gg/')
       .setStyle(ButtonStyle.Link);
 
     const button3 = new ButtonBuilder()
       .setLabel('Code')
-      .setURL('https://replit.com')
+      .setURL('https://replit.com/')
       .setStyle(ButtonStyle.Link);
       
     const row = new ActionRowBuilder()
